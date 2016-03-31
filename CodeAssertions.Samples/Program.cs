f29231841f40476fccd0a33b4dde095051dd3c18
ig-sinicyn <igor.sinicyn@gmail.com>
2016-03-31 08:45:52 +0300

    Starting sample

---
+++ CodeAssertions.Samples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeJam;
+
+namespace CodeAssertions.Samples
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var evenNumber = 11; // bad value
+
+			Code.AssertState(evenNumber % 2 == 0, "You shall not pass!");
+
+			Console.WriteLine($"{evenNumber} is even.");
+
+			Console.WriteLine("\r\nDone.");
+			Console.ReadKey();
+		}
+	}
+}
